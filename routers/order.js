9e6213aaeb321195be25b034c40b658cc7e5876c
Relylz <1926784738@qq.com>
2023-04-14 19:49:57 +0800

    添加了图片接口

--- routers/order.js
+++ routers/order.js
@@ -6,16 +6,16 @@ const orderDetail = require("../controllers/orderController/orderDetail");
 const orderList = require("../controllers/orderController/orderList");
 const orderSubmitComment = require("../controllers/orderController/orderSubmitComment");
 
-router.post("/getApplyReasonList", applyService.getApplyReasonList);
-router.post("/getApplyService", applyService.getApplyService);
-router.post("/getRightsPreview", applyService.getRightsPreview);
-router.post("/getSettleDetail", orderConfirm.getSettleDetail);
-router.post("/getCommitPay", orderConfirm.getCommitPay);
-router.post("/getSupplementInvoice", orderConfirm.getSupplementInvoice);
-router.post("/getBusinessTime", orderDetail.getBusinessTime);
-router.post("/getOrderDetail", orderDetail.getOrderDetail);
-router.post("/getOrders", orderList.getOrders);
-router.post("/getOrdersCount", orderList.getOrdersCount);
+router.post("/getApplyReasonList", applyService.getApplyReasonList); //搞定
+router.post("/getApplyService", applyService.getApplyService); //搞定
+router.post("/getRightsPreview", applyService.getRightsPreview); //暂定
+router.post("/getSettleDetail", orderConfirm.getSettleDetail); //解决
+router.post("/getCommitPay", orderConfirm.getCommitPay); //解决
+router.post("/getSupplementInvoice", orderConfirm.getSupplementInvoice); //可以不动
+router.post("/getBusinessTime", orderDetail.getBusinessTime); //解决
+router.post("/getOrderDetail", orderDetail.getOrderDetail); //解决
+router.post("/getOrders", orderList.getOrders); //已解决
+router.post("/getOrdersCount", orderList.getOrdersCount); //解决
 router.post("/getCommentGoods", orderSubmitComment.getCommentGoods);
 
 module.exports = router;
